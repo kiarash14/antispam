7d3e51c5cda3995c666a6d42729caaa841cc8486
mohammad2016mr <mohammad.kingme@gmail.com>
2016-11-13 13:33:51 -0800

    Create dati.php

---
+++ dati.php
@@ -0,0 +1,12 @@
+<?php
+
+//APIKEY
+$apiKey = "278007787:AAEfcg_h8SwK6VAYGSOH1MBYFVc1XXx5-_A";
+//TUOID
+$inserisciIlTuoId = "90516804,172561830";
+//puoi trovare il tuo id mandando un messaggio
+//a @userinfobot
+
+//BOT CREATO DA @xMicky24YT PER QUALSIASI PROBLEMA NON ESITARE A CONTATTARMI
+
+?>
